258af97d4516cabae21afc70388063a21bc79298
Aliyah Gant <aliyahgant5@gmail.com>
2024-05-15 22:25:54 -0400

    yayy

--- reviews.py
+++ reviews.py
@@ -1,2 +1,12 @@
-# add your code here
+import zipfile
+import pandas as pd
+
+df_wine_reviews = pd.read_csv("data/winemag-data-130k-v2.csv.zip", compression="zip", index_col=2)
+df_wine_countries_count_points = df_wine_reviews.groupby("country").agg({"country" : "count","points" : "mean"}).round(1)
+df_wine_countries_count_points = df_wine_countries_count_points.rename(columns={'country' : 'count'}).reset_index()
+df_wine_countries_summary = df_wine_countries_count_points.sort_values("count", ascending=False)
+
+#df_wine_countries_summary
+#a dataframe requires an index (default or not), the index can be removed/hidden in the function where you are writing to a file.
+df_wine_countries_summary.to_csv('reviews-per-country.csv', index=False)
 
